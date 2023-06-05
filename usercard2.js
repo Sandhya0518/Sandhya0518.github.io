a43840c5b2646f3405e226f3a90eaf83ef8343c9
mandlolla sandhya <93859004+Sandhya0518@users.noreply.github.com>
2023-06-05 22:28:19 +0530

    user js

---
+++ usercard2.js
@@ -0,0 +1,5 @@
+let Viratinfo{
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Sachin-Tendulkar_%28cropped%29.jpg/330px-Sachin-Tendulkar_%28cropped%29.jpg",
+    name: "sachin tendulkar",
+    Description: Indian Cricketer
+}
