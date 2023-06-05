cea95e0a36f8bee6dc272fbb27b371623d6f9a28
mandlolla sandhya <93859004+Sandhya0518@users.noreply.github.com>
2023-06-05 22:32:41 +0530

    user js

--- usercard2.js
+++ usercard2.js
@@ -3,3 +3,25 @@ let Viratinfo{
     name: "sachin tendulkar",
     Description: Indian Cricketer
 }
+let yuvrajnewinfo = {
+    imgurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Sachin-Tendulkar_%28cropped%29.jpg/330px-Sachin-Tendulkar_%28cropped%29.jpg"
+    name: "sachin tendulkar",
+    description: "Man of the Series - World Cup 2011"
+}
+
+let issachinold = true;
+let displayobject;
+
+let flipdata = function(){
+    if(issachinold == true){
+        displayobject = sachinnewinfo;
+        issachinold = false;
+    }
+    else{
+        displayobject = sachinoldinfo;
+        issachinold = true;
+    }
+    document.getElementById("ywc").src=displayobject.imgurl;
+    document.getElementById("ywc-name").innerHTML=displayobject.name;
+    document.getElementById("ywc-des").innerHTML = displayobject.description;
+}
