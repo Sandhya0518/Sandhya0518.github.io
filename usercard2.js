d9b06328dc21169cbf8a16cb91f43a3450408056
mandlolla sandhya <93859004+Sandhya0518@users.noreply.github.com>
2023-06-06 14:53:31 +0530

    user2 js

--- usercard2.js
+++ usercard2.js
@@ -4,8 +4,7 @@ let sachininfo{
     Description: Indian Cricketer
 }
 let sachinnewinfo = {
-    imgurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Sachin-Tendulkar_%28cropped%29.jpg/330px-Sachin-Tendulkar_%28cropped%29.jpg"
-    name: "sachin tendulkar",
+    imgurl: "https://staticg.sportskeeda.com/editor/2018/03/097bd-1521526789-800.jpg"
     description: "Man of the Series - World Cup 2011"
 }
 
