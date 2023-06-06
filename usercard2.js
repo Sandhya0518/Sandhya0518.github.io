5dee674d0ab5de84af0d4733d6c16857ce4596ed
mandlolla sandhya <93859004+Sandhya0518@users.noreply.github.com>
2023-06-06 15:17:46 +0530

    user2

--- usercard2.js
+++ usercard2.js
@@ -1,11 +1,12 @@
 let sachininfo = {
     imgurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Sachin-Tendulkar_%28cropped%29.jpg/330px-Sachin-Tendulkar_%28cropped%29.jpg",
     name: "sachin tendulkar",
-    Description: Indian Cricketer
+    Description: "Indian Cricketer"
 }
 let sachinnewinfo = {
-    imgurl: "https://staticg.sportskeeda.com/editor/2018/03/097bd-1521526789-800.jpg"
-    description: "Man of the Series - World Cup 2011"
+    imgurl: "https://staticg.sportskeeda.com/editor/2018/03/097bd-1521526789-800.jpg",
+    name: "sachin tendulkar",
+    description: "He has carried the burden of the nation for 21 years"
 }
 
 let issachinold = true;
