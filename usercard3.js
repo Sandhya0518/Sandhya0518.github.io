cbf9277e8209654d2523109ea61ba0b30d2d9368
mandlolla sandhya <93859004+Sandhya0518@users.noreply.github.com>
2023-06-13 21:58:07 +0530

    user3

--- usercard3.js
+++ usercard3.js
@@ -3,24 +3,14 @@ let sachininfo = {
     name: "sachin tendulkar",
     Description: "Indian Cricketer"
 }
-let sachinnewinfo = {
-    imgurl: "https://staticg.sportskeeda.com/editor/2018/03/097bd-1521526789-800.jpg",
-    name: "sachin tendulkar",
-    description: "He has carried the burden of the nation for 21 years"
-}
 
-let issachinold = true;
 let displayobject;
 
-let flipdata = function(){
-    if(issachinold == true){
-        displayobject = sachinnewinfo;
-        issachinold = false;
-    }
-    else{
-        displayobject = sachinoldinfo;
-        issachinold = true;
-    }
+let getRandomUser = function(){
+    fetch("https://randomuser.me/api")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    
     document.getElementById("ywc").src=displayobject.imgurl;
     document.getElementById("ywc-name").innerHTML=displayobject.name;
     document.getElementById("ywc-des").innerHTML = displayobject.description;
